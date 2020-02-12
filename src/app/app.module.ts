0f354dd3e02b7558d5c41aa49301469186a9ed04
Jan Sommer <jan.sommer@hsrw.org>
2020-02-12 18:12:31 +0100

    Removes blog module and adjusts settings for web workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RoutingModule } from './routing/routing.module';
-import { BlogModule } from './blog/blog/blog.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { FooterModule } from "./footer/footer.module";
+import { RoutingModule } from "./routing/routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     CoreModule,
-    BlogModule,
+    FooterModule,
     RoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { BlogComponent } from '../blog/blog/blog.component';
 import { LegalNoticeComponent } from '../core/components/legal-notice/legal-notice.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "/", pathMatch: "full" },
   { path: "legal-notice", component: LegalNoticeComponent },
-  { path: "blog", component: BlogComponent }
 ];
