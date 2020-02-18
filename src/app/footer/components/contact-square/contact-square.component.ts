0ca1895de98b1344ec57af2840162ed49d4f11a0
Jan Sommer <jan.sommer@hsrw.org>
2020-02-18 20:00:18 +0100

    Adds hover animation for contact squares,  places footer at the bottom and let the header stick

--- src/app/footer/components/contact-square/contact-square.component.ts
+++ src/app/footer/components/contact-square/contact-square.component.ts
@@ -1,11 +1,33 @@
 import { Component, Input } from "@angular/core";
 
 import { ContactProvider } from "../contact-provider";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  query,
+  animateChild
+} from "@angular/animations";
 
 @Component({
   selector: "app-contact-square",
   templateUrl: "./contact-square.component.html",
-  styleUrls: ["./contact-square.component.scss"]
+  styleUrls: ["./contact-square.component.scss"],
+  animations: [
+    trigger("hover", [
+      state(
+        "hoverIn",
+        style({
+          backgroundColor: "tomato",
+        })
+      ),
+      state("hoverOut", style({})),
+      transition("hoverIn => hoverOut", [animate("0.3s")]),
+      transition("hoverOut => hoverIn", [animate("0.5s")])
+    ])
+  ]
 })
 export class ContactSquareComponent {
   @Input()
@@ -13,6 +35,8 @@ export class ContactSquareComponent {
 
   @Input()
   public contactProvider: ContactProvider;
+
+  public isHovering = false;
 
   public onClick(): void {
     window.location.href = this.targetUrl;
