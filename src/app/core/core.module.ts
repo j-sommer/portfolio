2b5fd6eb5927a8d433b53f080ee805469d50d282
Jan Sommer <jan.sommer@hsrw.org>
2020-03-20 20:22:08 +0100

    installs aos

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,20 +1,21 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { RoutingModule } from '../routing/routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { LegalNoticeComponent } from './components/legal-notice/legal-notice.component';
+import { RoutingModule } from "../routing/routing.module";
+import { HeaderComponent } from "./components/header/header.component";
+import { LegalNoticeComponent } from "./components/legal-notice/legal-notice.component";
 
 @NgModule({
   declarations: [HeaderComponent, LegalNoticeComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
+    MatCardModule,
     MatButtonModule,
-    RoutingModule,
+    RoutingModule
   ],
   exports: [HeaderComponent]
 })
