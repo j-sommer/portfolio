90194ccc5c520e751e2611bdf9a835178ed1eb26
Jan Sommer <jan-sommer@outlook.com>
2020-05-03 20:20:05 +0200

    Fix failing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { TranslateModule } from "@ngx-translate/core";
 import { MockComponents } from "ng-mocks";
 
 import { AppComponent } from "./app.component";
+import { ContactModule } from "./contact/contact.module";
 import { HeaderComponent } from "./core/components/header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { TranslateModule } from "@ngx-translate/core";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
@@ -14,7 +15,7 @@ describe("AppComponent", () => {
         AppComponent,
         MockComponents(HeaderComponent, FooterComponent),
       ],
-      imports: [RouterTestingModule, TranslateModule.forRoot()],
+      imports: [RouterTestingModule, ContactModule, TranslateModule.forRoot()],
     }).compileComponents();
   }));
 
