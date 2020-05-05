30a1c1dc8829dbe9e57910090566e7e6c923c02d
Jan Sommer <jan-sommer@outlook.com>
2020-05-05 21:25:22 +0200

    Insert menu entries into toolbar

---
+++ src/app/routing/models/route-options.ts
@@ -0,0 +1,7 @@
+import { ExtraOptions } from "@angular/router";
+
+export const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: "enabled",
+  anchorScrolling: "enabled",
+  scrollOffset: [0, 32],
+};
