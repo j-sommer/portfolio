3dc73fbfbe292745bbb3c8e794df51ceea70000e
Jan Sommer <jan-sommer@outlook.com>
2020-05-13 20:35:44 +0200

    Implement scroll to top button

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 import { FooterComponent } from "./footer.component";
+import { By } from "@angular/platform-browser";
 
 describe("FooterComponent", () => {
   let component: FooterComponent;
@@ -9,6 +12,7 @@ describe("FooterComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [FooterComponent],
+      imports: [MatButtonModule, MatIconModule],
     }).compileComponents();
   }));
 
@@ -20,5 +24,20 @@ describe("FooterComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should contain a button for scrolling to the top when created", () => {
+    // Given
+    const scrollToSpy = jest
+      .spyOn(window, "scrollTo")
+      .mockImplementation(() => {});
+
+    // When
+    const buttonElement = fixture.debugElement.query(By.css("button"));
+    buttonElement.nativeElement.click();
+
+    // Then
+    expect(buttonElement).toBeTruthy();
+    expect(scrollToSpy).toHaveBeenCalledWith({ behavior: "smooth", top: 0 });
   });
 });
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,4 +5,8 @@ import { Component } from "@angular/core";
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.scss"],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+}
