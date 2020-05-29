df42817b6d1eb3aeb54a3a8b4897684e44fb7162
Jan Sommer <jan-sommer@outlook.com>
2020-05-29 15:52:17 +0200

    draft api call

--- src/app/contact/services/contact.service.spec.ts
+++ src/app/contact/services/contact.service.spec.ts
@@ -1,30 +1,11 @@
 import { ContactService } from "./contact.service";
-import { ContactRequest } from "../models/contact-request.model";
-import { HttpClient } from "@angular/common/http";
 
 describe("ContactService", () => {
-  const mockHttpClient = {
-    post: jest.fn(),
-  } as unknown;
-
   let service: ContactService;
 
   beforeEach(() => {
-    service = new ContactService(mockHttpClient as HttpClient);
+    service = new ContactService();
   });
 
-  it("should send the contact request via POST when invoked", () => {
-    // Given
-    const mockContactRequest = {
-      name: "name",
-      email: "test@test.com",
-      message: "message",
-    } as ContactRequest;
-
-    // When
-    const actual = service.sendContactRequest(mockContactRequest);
-
-    // Then
-    expect(actual);
-  });
+  it.todo("should send the contact request via POST when invoked");
 });
--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,16 +1,15 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
+import API from "@aws-amplify/api";
+import { from, Observable } from "rxjs";
 
 import { ContactRequest } from "../models/contact-request.model";
 
 @Injectable()
 export class ContactService {
-  private readonly apiUrl = "http://localhost:3000/contact";
-
-  constructor(private httpClient: HttpClient) {}
+  private readonly apiName = "contact";
+  private readonly path = "/contact";
 
   public sendContactRequest(contactRequest: ContactRequest): Observable<any> {
-    return this.httpClient.post(this.apiUrl, contactRequest);
+    return from(API.post(this.apiName, this.path, { body: contactRequest }));
   }
 }
