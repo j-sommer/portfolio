b6b21b4931945761c797a89a7337a6cc14045cee
Jan Sommer <jan-sommer@outlook.com>
2021-03-13 17:41:11 +0100

    Use aws api gateway for contact requests

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,15 +1,16 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import API from "@aws-amplify/api";
-import { from, Observable } from "rxjs";
-
+import { Observable } from "rxjs";
 import { ContactRequest } from "../models/contact-request.model";
 
 @Injectable()
 export class ContactService {
-  private readonly apiName = "contact";
-  private readonly path = "/contact";
+  private readonly apiBaseUrl =
+    "https://jgqnm1ecf5.execute-api.eu-west-1.amazonaws.com/prod";
+
+  constructor(private http: HttpClient) {}
 
   public sendContactRequest(contactRequest: ContactRequest): Observable<any> {
-    return from(API.post(this.apiName, this.path, { body: contactRequest }));
+    return this.http.post<ContactRequest>(this.apiBaseUrl, contactRequest);
   }
 }
