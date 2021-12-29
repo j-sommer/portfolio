89a5ed16b7c98f2c4b88e583720696af1149647f
Jan Sommer <jan-sommer@outlook.com>
2021-12-29 21:17:31 +0100

    install package for fetching medium articles

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
 import { SectionComponent } from "./components/section/section.component";
+import { MediumWidgetModule } from "ngx-medium-widget";
 
 @NgModule({
   declarations: [SectionComponent],
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, MediumWidgetModule],
   exports: [SectionComponent],
 })
 export class BlogModule {}
