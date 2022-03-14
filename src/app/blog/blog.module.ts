51b846e7f3679242a5ca7f718bbc810e1b64438f
Jan Sommer <jan-sommer@outlook.com>
2022-03-14 17:52:32 +0100

    Remove broken dependency

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
+
 import { SectionComponent } from "./components/section/section.component";
-import { MediumWidgetModule } from "ngx-medium-widget";
 
 @NgModule({
   declarations: [SectionComponent],
-  imports: [CommonModule, TranslateModule, MediumWidgetModule],
+  imports: [CommonModule, TranslateModule],
   exports: [SectionComponent],
 })
 export class BlogModule {}
