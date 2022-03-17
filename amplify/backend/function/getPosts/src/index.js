d08cb01965cd204918bebb6a751c9ebfde9c25d6
Jan Sommer <jan-sommer@outlook.com>
2022-03-17 18:48:10 +0100

    Update aws lambda function

--- amplify/backend/function/getPosts/src/index.js
+++ amplify/backend/function/getPosts/src/index.js
@@ -10,11 +10,10 @@ exports.handler = async (event) => {
   console.log(`FEED: ${JSON.stringify(feed)}`);
   return {
     statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
     body: JSON.stringify(feed),
   };
 };
