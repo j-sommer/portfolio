52798879e521df739a1079026e54a9dbd3115790
Jan Sommer <jan-sommer@outlook.com>
2022-03-17 18:48:53 +0100

    Display stories feed in blog section

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
 
 import { SectionComponent } from "./components/section/section.component";
+import { BlogService } from "./services/blog.service";
 
 @NgModule({
   declarations: [SectionComponent],
   imports: [CommonModule, TranslateModule],
   exports: [SectionComponent],
+  providers: [BlogService],
 })
 export class BlogModule {}
