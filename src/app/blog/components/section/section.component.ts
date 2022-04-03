0963d3e787c61373021fa62b0c1c498c387b4251
Jan Sommer <jan-sommer@outlook.com>
2022-04-03 14:37:06 +0200

    blog section styling adjustments

--- src/app/blog/components/section/section.component.ts
+++ src/app/blog/components/section/section.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-import { Item, MediumFeed } from "medium-stories-fetcher";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
 import { BlogService } from "../../services/blog.service";
+
+export interface BlogItem {
+  title: string;
+  imageElem: string;
+  descriptionElem: string;
+  linkElem: string;
+}
 
 @Component({
   selector: "app-blog-section",
@@ -11,11 +17,42 @@ import { BlogService } from "../../services/blog.service";
   styleUrls: ["./section.component.scss"],
 })
 export class SectionComponent implements OnInit {
-  public posts: Observable<Item[]>;
+  public posts: Observable<BlogItem[]>;
+
+  private parser = new DOMParser();
 
   constructor(private blogService: BlogService) {}
 
   public ngOnInit(): void {
-    this.posts = this.blogService.getPosts().pipe(map((feed) => feed.items));
+    this.posts = this.blogService
+      .getPosts()
+      .pipe(map((feed) => feed.items))
+      .pipe(
+        map((items) => {
+          return items.map((item) => {
+            const itemDOM = this.parser.parseFromString(
+              item.description,
+              "text/html"
+            );
+
+            const imageElem = itemDOM
+              .getElementsByClassName("medium-feed-image")
+              .item(0).innerHTML;
+            const descriptionElem = itemDOM
+              .getElementsByClassName("medium-feed-snippet")
+              .item(0).innerHTML;
+            const linkElem = itemDOM
+              .getElementsByClassName("medium-feed-link")
+              .item(0).innerHTML;
+
+            return {
+              title: item.title,
+              imageElem,
+              descriptionElem,
+              linkElem,
+            } as BlogItem;
+          });
+        })
+      );
   }
 }
