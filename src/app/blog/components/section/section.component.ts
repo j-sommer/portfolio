8589e23928fb97d6c3baff510f37eb4bfc3e935e
Jan Sommer <jan-sommer@outlook.com>
2022-04-11 07:33:21 +0200

    Fetch and parse new medium feed structure

--- src/app/blog/components/section/section.component.ts
+++ src/app/blog/components/section/section.component.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
 
+import { BlogItem } from "../../models/blog-item.model";
 import { BlogService } from "../../services/blog.service";
-
-export interface BlogItem {
-  title: string;
-  imageElem: string;
-  descriptionElem: string;
-  linkElem: string;
-}
 
 @Component({
   selector: "app-blog-section",
@@ -19,40 +12,9 @@ export interface BlogItem {
 export class SectionComponent implements OnInit {
   public posts: Observable<BlogItem[]>;
 
-  private parser = new DOMParser();
-
   constructor(private blogService: BlogService) {}
 
   public ngOnInit(): void {
-    this.posts = this.blogService
-      .getPosts()
-      .pipe(map((feed) => feed.items))
-      .pipe(
-        map((items) => {
-          return items.map((item) => {
-            const itemDOM = this.parser.parseFromString(
-              item.description,
-              "text/html"
-            );
-
-            const imageElem = itemDOM
-              .getElementsByClassName("medium-feed-image")
-              .item(0).innerHTML;
-            const descriptionElem = itemDOM
-              .getElementsByClassName("medium-feed-snippet")
-              .item(0).innerHTML;
-            const linkElem = itemDOM
-              .getElementsByClassName("medium-feed-link")
-              .item(0).innerHTML;
-
-            return {
-              title: item.title,
-              imageElem,
-              descriptionElem,
-              linkElem,
-            } as BlogItem;
-          });
-        })
-      );
+    this.posts = this.blogService.getPosts();
   }
 }
--- src/app/blog/services/blog.service.ts
+++ src/app/blog/services/blog.service.ts
@@ -2,15 +2,54 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MediumFeed } from "medium-stories-fetcher";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+import { BlogItem } from "../models/blog-item.model";
 
 @Injectable()
 export class BlogService {
   private readonly apiBaseUrl =
     "https://lr9q2wpoil.execute-api.eu-west-1.amazonaws.com/master/posts";
 
+  private parser = new DOMParser();
+
   constructor(private http: HttpClient) {}
 
-  public getPosts(): Observable<MediumFeed> {
-    return this.http.get<MediumFeed>(this.apiBaseUrl);
+  public getPosts(): Observable<BlogItem[]> {
+    return this.http.get<MediumFeed>(this.apiBaseUrl).pipe(
+      map((mediumFeed) => mediumFeed.items),
+      map((items) => {
+        return items.map((item) => {
+          const itemDOM = this.parser.parseFromString(
+            (item as any).content,
+            "text/html"
+          );
+
+          const imageElem = itemDOM.getElementsByTagName("img").item(0)
+            .outerHTML;
+
+          const paragraphElements = itemDOM.getElementsByTagName("p");
+          let contentPreview;
+
+          const asArray = Array.from(paragraphElements);
+
+          if (paragraphElements.length > 2) {
+            contentPreview = asArray
+              .slice(0, 2)
+              .map((item) => item.outerHTML)
+              .join();
+          } else {
+            contentPreview = paragraphElements.item(0).outerHTML;
+          }
+
+          return {
+            imageElem,
+            title: item.title,
+            link: item.link,
+            contentPreview,
+          };
+        });
+      })
+    );
   }
 }
